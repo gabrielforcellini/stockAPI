d30f9cd66db45ffd49cc633072678ca4ea2202f3
Gabriel <gabrielpavan2014@outlook.com>
2023-05-14 22:41:46 -0300

    Outras correções

--- src/config/_database.js
+++ src/config/_database.js
@@ -1,10 +1,15 @@
 import pg from 'pg';
 
+const DB_USER = process.env.DB_USER;
+const DB_SERVER = process.env.DB_SERVER;
+const DB_NAME = process.env.DB_NAME;
+const DB_PASSWORD = process.env.DB_PASSWORD;
+
 const client = new pg.Client({
-  user: 'postgres',
-  database: 'estoque',
-  host: 'localhost',
-  password: 'admin',
+  user: DB_USER,
+  database: DB_NAME,
+  host: DB_SERVER,
+  password: DB_PASSWORD,
   port: 5432,
 })
 
--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -42,8 +42,6 @@ export class UserController {
         complemento,
         ativo
         } = req.body;
-      
-      
     };
   }
 
